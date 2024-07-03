87a469cd5a75b2d996fb0ad36040d8a10d8665b4
Ben <ben@skyeyes.tw>
2024-07-03 10:11:47 +0800

    /* ben */ 1.更新底層

--- baselib/src/main/java/com/benwu/baselib/extension/shapeable/ShapeableConstraintLayout.kt
+++ baselib/src/main/java/com/benwu/baselib/extension/shapeable/ShapeableConstraintLayout.kt
@@ -2,13 +2,17 @@ package com.benwu.baselib.extension.shapeable
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.graphics.Outline
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewOutlineProvider
+import androidx.annotation.ColorInt
+import androidx.annotation.StyleRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.benwu.baselib.R
-import com.benwu.baselib.extension.getAttrValue
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
 
@@ -19,46 +23,97 @@ open class ShapeableConstraintLayout @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
 
+    private var strokeWidth: Float
+    private var strokeColor: Int
+    private var shapeAppearance: Int
+    private var shapeAppearanceOverlay: Int
+
+    private var shapeDrawable: MaterialShapeDrawable
+
     init {
         val attributes =
             context.obtainStyledAttributes(attrs, R.styleable.ShapeableConstraintLayout)
 
-        val strokeWidth =
-            attributes.getDimension(R.styleable.ShapeableConstraintLayout_strokeWidth, 0f)
-        val strokeColor = attributes.getColor(
+        strokeWidth = attributes.getDimension(R.styleable.ShapeableConstraintLayout_strokeWidth, 0f)
+        strokeColor = attributes.getColor(
             R.styleable.ShapeableConstraintLayout_strokeColor,
             Color.TRANSPARENT
         )
-        val shapeAppearance =
-            attributes.getResourceId(R.styleable.ShapeableConstraintLayout_shapeAppearance, 0)
-        val shapeAppearanceOverlay = attributes.getResourceId(
+        shapeAppearance = attributes.getResourceId(
+            R.styleable.ShapeableConstraintLayout_shapeAppearance,
+            com.google.android.material.R.style.ShapeAppearance_Material3_Corner_None
+        )
+        shapeAppearanceOverlay = attributes.getResourceId(
             R.styleable.ShapeableConstraintLayout_shapeAppearanceOverlay,
             0
         )
 
-        if (shapeAppearance != 0 || shapeAppearanceOverlay != 0) {
-            val color = when (val drawable = background) {
-                is ColorDrawable -> {
-                    drawable.color
-                }
+        shapeDrawable = buildBackground()
 
-                else -> {
-                    context.getAttrValue(com.google.android.material.R.attr.colorSurface)
-                }
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                shapeDrawable.bounds = Rect(0, 0, view.width, view.height)
+                shapeDrawable.getOutline(outline)
             }
-
-            background = MaterialShapeDrawable(
-                ShapeAppearanceModel.builder(context, shapeAppearance, shapeAppearanceOverlay)
-                    .build()
-            ).also {
-                it.setStroke(strokeWidth, strokeColor)
-                it.fillColor = ColorStateList.valueOf(color)
-            }
-
-            outlineProvider = ViewOutlineProvider.BACKGROUND
-            clipToOutline = true
         }
 
+        clipToOutline = true
+
         attributes.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        shapeDrawable.setBounds(0, 0, width, height)
+        shapeDrawable.draw(canvas)
+    }
+
+    /**
+     * 創建背景
+     *
+     * @return 背景
+     */
+    private fun buildBackground() = MaterialShapeDrawable(
+        ShapeAppearanceModel.builder(context, shapeAppearance, shapeAppearanceOverlay)
+            .build()
+    ).also {
+        it.setStroke(strokeWidth, strokeColor)
+        it.fillColor = ColorStateList.valueOf(Color.TRANSPARENT)
+    }
+
+    /**
+     * 設置邊框
+     *
+     * @param width 寬度
+     * @param color 顏色
+     */
+    fun setStroke(width: Float, @ColorInt color: Int) {
+        strokeWidth = width
+        strokeColor = color
+        shapeDrawable.setStroke(strokeWidth, strokeColor)
+        invalidate()
+    }
+
+    /**
+     * 設置形狀
+     *
+     * @param shape 形狀
+     */
+    fun setShapeAppearance(@StyleRes shape: Int) {
+        shapeAppearance = shape
+        shapeDrawable = buildBackground()
+        invalidate()
+    }
+
+    /**
+     * 設置覆蓋形狀
+     *
+     * @param shape 形狀
+     */
+    fun setShapeAppearanceOverlay(@StyleRes shape: Int) {
+        shapeAppearanceOverlay = shape
+        shapeDrawable = buildBackground()
+        invalidate()
     }
 }
--- baselib/src/main/java/com/benwu/baselib/extension/shapeable/ShapeableTextView.kt
+++ baselib/src/main/java/com/benwu/baselib/extension/shapeable/ShapeableTextView.kt
@@ -2,12 +2,16 @@ package com.benwu.baselib.extension.shapeable
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.graphics.Outline
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewOutlineProvider
+import androidx.annotation.ColorInt
+import androidx.annotation.StyleRes
 import com.benwu.baselib.R
-import com.benwu.baselib.extension.getAttrValue
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
 import com.google.android.material.textview.MaterialTextView
@@ -18,40 +22,92 @@ open class ShapeableTextView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : MaterialTextView(context, attrs, defStyleAttr) {
 
+    private var strokeWidth: Float
+    private var strokeColor: Int
+    private var shapeAppearance: Int
+    private var shapeAppearanceOverlay: Int
+
+    private var shapeDrawable: MaterialShapeDrawable
+
     init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.ShapeableTextView)
 
-        val strokeWidth = attributes.getDimension(R.styleable.ShapeableTextView_strokeWidth, 0f)
-        val strokeColor =
+        strokeWidth = attributes.getDimension(R.styleable.ShapeableTextView_strokeWidth, 0f)
+        strokeColor =
             attributes.getColor(R.styleable.ShapeableTextView_strokeColor, Color.TRANSPARENT)
-        val shapeAppearance =
-            attributes.getResourceId(R.styleable.ShapeableTextView_shapeAppearance, 0)
-        val shapeAppearanceOverlay =
+        shapeAppearance = attributes.getResourceId(
+            R.styleable.ShapeableTextView_shapeAppearance,
+            com.google.android.material.R.style.ShapeAppearance_Material3_Corner_None
+        )
+        shapeAppearanceOverlay =
             attributes.getResourceId(R.styleable.ShapeableTextView_shapeAppearanceOverlay, 0)
 
-        if (shapeAppearance != 0 || shapeAppearanceOverlay != 0) {
-            val color = when (val drawable = background) {
-                is ColorDrawable -> {
-                    drawable.color
-                }
+        shapeDrawable = buildBackground()
 
-                else -> {
-                    context.getAttrValue(com.google.android.material.R.attr.colorSurface)
-                }
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                shapeDrawable.bounds = Rect(0, 0, view.width, view.height)
+                shapeDrawable.getOutline(outline)
             }
-
-            background = MaterialShapeDrawable(
-                ShapeAppearanceModel.builder(context, shapeAppearance, shapeAppearanceOverlay)
-                    .build()
-            ).also {
-                it.setStroke(strokeWidth, strokeColor)
-                it.fillColor = ColorStateList.valueOf(color)
-            }
-
-            outlineProvider = ViewOutlineProvider.BACKGROUND
-            clipToOutline = true
         }
 
+        clipToOutline = true
+
         attributes.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        shapeDrawable.setBounds(0, 0, width, height)
+        shapeDrawable.draw(canvas)
+    }
+
+    /**
+     * 創建背景
+     *
+     * @return 背景
+     */
+    private fun buildBackground() = MaterialShapeDrawable(
+        ShapeAppearanceModel.builder(context, shapeAppearance, shapeAppearanceOverlay)
+            .build()
+    ).also {
+        it.setStroke(strokeWidth, strokeColor)
+        it.fillColor = ColorStateList.valueOf(Color.TRANSPARENT)
+    }
+
+    /**
+     * 設置邊框
+     *
+     * @param width 寬度
+     * @param color 顏色
+     */
+    fun setStroke(width: Float, @ColorInt color: Int) {
+        strokeWidth = width
+        strokeColor = color
+        shapeDrawable.setStroke(strokeWidth, strokeColor)
+        invalidate()
+    }
+
+    /**
+     * 設置形狀
+     *
+     * @param shape 形狀
+     */
+    fun setShapeAppearance(@StyleRes shape: Int) {
+        shapeAppearance = shape
+        shapeDrawable = buildBackground()
+        invalidate()
+    }
+
+    /**
+     * 設置覆蓋形狀
+     *
+     * @param shape 形狀
+     */
+    fun setShapeAppearanceOverlay(@StyleRes shape: Int) {
+        shapeAppearanceOverlay = shape
+        shapeDrawable = buildBackground()
+        invalidate()
     }
 }
